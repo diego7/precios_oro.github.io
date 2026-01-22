9be9695e3e4f4a4c04f21085da9ddea73186cf69
diego7 <diegosullca9@gmail.com>
2026-01-22 19:04:04 -0400

    Add files via upload

--- js/precios.js
+++ js/precios.js
@@ -1,51 +1,19 @@
 import { db } from "./firebase.js";
-import { ref, set, get } from
+import { ref, set } from
   "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
-/* CONFIG */
-const LEY_MIN = 75;
-const LEY_MAX = 99;
-const ONZA_API = "https://www.goldapi.io/api/XAU/USD";
-const API_KEY = "goldapi-cxixsmkoojobf-io"; // 🔴 pon tu key real
+console.log("precios.js cargado");
 
-/* OBTENER ONZA */
-async function obtenerOnza() {
-  const res = await fetch(ONZA_API, {
-    headers: { "x-access-token": goldapi-cxixsmkoojobf-io }
-  });
-  const data = await res.json();
-  return data.price;
-}
-
-/* CALCULAR TODAS LAS LEYES */
-function calcularPrecios(onza, dolar, descuento) {
-  const precios = {};
-
-  for (let ley = LEY_MIN; ley <= LEY_MAX; ley++) {
-    const gramoUsd = (onza / 31.1035) * (ley / 100);
-    const gramoBs = gramoUsd * dolar;
-    const finalBs = gramoBs * (1 - descuento / 100);
-
-    precios[`ley_${ley}`] = {
-      usd: gramoUsd.toFixed(2),
-      bs: finalBs.toFixed(2)
-    };
-  }
-
-  return precios;
-}
-
-/* PROCESO COMPLETO */
 export async function actualizarPrecios() {
-  const snap = await get(ref(db, "config"));
-  if (!snap.exists()) return;
+  console.log("actualizarPrecios ejecutado");
 
-  const { dolar, descuento } = snap.val();
+  const preciosTest = {
+    ley_75: { usd: "100", bs: "700" },
+    ley_76: { usd: "101", bs: "707" }
+  };
 
-  const onza = await obtenerOnza();
+  await set(ref(db, "precios"), preciosTest);
+  await set(ref(db, "config/ultima_onza"), 9999);
 
-  await set(ref(db, "config/ultima_onza"), onza);
-
-  const precios = calcularPrecios(onza, dolar, descuento);
-  await set(ref(db, "precios"), precios);
+  console.log("datos escritos en firebase");
 }
