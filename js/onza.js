80e6040a1e899d84bb75de5058d6297b3f3c4952
diego7 <diegosullca9@gmail.com>
2026-01-23 21:25:10 -0400

    Update onza.js

--- js/onza.js
+++ js/onza.js
@@ -3,11 +3,11 @@ export async function obtenerOnzaTroy() {
     const res = await fetch("https://api.metals.live/v1/spot/gold");
     const data = await res.json();
 
-    // formato: [["gold", precio, timestamp]]
+    // Formato: [["gold", precio, timestamp]]
     return Number(data[0][1]);
 
-  } catch (err) {
-    console.error("Error obteniendo onza", err);
+  } catch (e) {
+    console.error("Error obteniendo onza troy", e);
     return null;
   }
 }
